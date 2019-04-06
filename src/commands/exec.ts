6ed24b0865e0dcaecb2d09334c9e5a8f4e6e68bb
Arvin <arvin.rokni@yahoo.com>
2019-04-06 19:37:49 +0430

    exec

--- src/commands/exec.ts
+++ src/commands/exec.ts
@@ -1,9 +1,9 @@
 import {Command, flags} from '@oclif/command'
+import {string} from '@oclif/command/lib/flags'
 import axios from 'axios'
-
-let request = require('request')
-
-const httpAdapter = require('axios/lib/adapters/http')
+import {socket} from 'axon'
+import {Duplex} from 'stream'
+let readline = require('readline')
 
 export default class Exec extends Command {
   static description = 'execute command on instance'
@@ -14,10 +14,24 @@ export default class Exec extends Command {
   ]
 
   static flags = {
-    help: flags.help({char: 'h'})
+    help: flags.help({char: 'h'}),
+    interactive: flags.boolean({char: 'i'})
   }
 
-  static args = [{name: 'file'}]
+  static args = [
+    {
+      name: 'app',
+      required: true,
+      description: 'app id/name',
+      hidden: false
+    }, {
+      name: 'cmd',
+      required: false,
+      description: 'command',
+      hidden: false,
+      default: 'bash'
+    }
+  ]
 
   async run() {
     const {args, flags} = this.parse(Exec)
@@ -26,14 +40,61 @@ export default class Exec extends Command {
       AttachStdin: true,
       AttachStdout: true,
       AttachStderr: true,
-      DetachKeys: 'ctrl-p,ctrl-q',
+      DetachKeys: 'ctrl-c,ctrl-q',
       Tty: true,
       Cmd: [
-        'bash'
+        args.cmd
       ],
       Env: []
     }
+    let rl = readline.createInterface(process.stdin, process.stdout)
+    let id = await axios.post(`http://127.0.0.1:2375/containers/${args.app}/exec`, data)
+      .then(value => value.data.Id)
+    if (flags.interactive || args.cmd === 'bash') {
+      axios.post(`http://127.0.0.1:2375/exec/${id}/start`,
+        {
+          Detach: false,
+          Tty: true
+        }, {
+          responseType: 'stream'
+        }).then(response => {
+          let stream = response.data
+          let socket = stream.socket
 
+          socket.on('error', (data: string) => {
+            process.stdin.pause()
+            process.stderr.write(data)
+            process.stdin.resume()
+          })
+
+          socket.on('data', (data: string) => {
+            process.stdin.pause()
+            process.stdout.write(data)
+            process.stdin.resume()
+          })
+
+          rl.on('line', (input: string) => {
+            socket.write(input.trim() + '\n')
+          })
+
+          rl.on('SIGINT', function () {
+            // stop input
+            process.stdin.pause()
+            process.stderr.write('\nEnding session\n')
+            rl.close()
+            socket.emit('end')
+          })
+        })
+    } else {
+      axios.post<string>(`http://127.0.0.1:2375/exec/${id}/start`,
+        {
+          Detach: false,
+          Tty: true
+        }).then(response => {
+          rl.write(response.data)
+          rl.close()
+        })
+    }
   }
 }
 
