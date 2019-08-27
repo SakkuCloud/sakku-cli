6e5acbfcf8712d08fd5a33e8237e4331eb2ca576
e.shekari <e.shekari@gmail.com>
2019-08-27 10:12:33 +0430

    catalog finished. error handling for connection errors added

--- src/_service/exec.service.ts
+++ src/_service/exec.service.ts
@@ -1,7 +1,11 @@
-import {Command} from '@oclif/command';
+// External Modules
+import { Command } from '@oclif/command';
 import axios from 'axios';
 
-import {readToken} from '../utils/read-token';
+// Project Modules
+import { readToken } from '../utils/read-token';
+import { common } from '../utils/common';
+
 
 export const execService = {
   create,
@@ -9,15 +13,21 @@ export const execService = {
 };
 
 function create(ctx: Command, data: {}, host: string, cid: string) {
-  return axios.post(host + '/containers/' + cid + '/exec', data, {headers: getHeader(ctx)})
-    .then(value => value.data.Id);
+  return axios.post(host + '/containers/' + cid + '/exec', data, { headers: getHeader(ctx) })
+    .then(value => value.data.Id)
+    .catch((error) => {
+      throw common.handleRequestError(error);
+    });
 }
 
 function exec(ctx: Command, id: string, host: string, data: {}) {
   return axios.post(host + `/exec/${id}/start`,
-    data, {responseType: 'stream'});
+    data, { responseType: 'stream' })
+    .catch((error) => {
+      throw common.handleRequestError(error);
+    });;
 }
 
 function getHeader(ctx: Command) {
-  return {Authorization: readToken(ctx)};
+  return { Authorization: readToken(ctx) };
 }
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -12,7 +12,7 @@ const handleRequestError = (err: any, isLogin: boolean = false) => {
     if (isLogin && standardError.code && (standardError.code == '403' || standardError.code == '400')) {
       standardError.message = messages.incorrect_credentials;
     }
-    else if (err.response && err.response.data) {
+    else if (err.response && err.response.data && err.response.data.message) {
       standardError.message = err.response.data.message || '';
     }
     else if (err.response && err.response.statusText) {
@@ -31,14 +31,18 @@ const checkStandardError = (err: any) => {
 }
 
 const logError = (err: { code: number | null | string, message: string }) => {
+  let errorMessage: string = 'An Error Occured! ';
   if (checkStandardError(err)) {
-    let errorMessage: string = 'An Error Occured! ';
     if (err.hasOwnProperty('code')) {
       errorMessage += `Error code is: ${err.code}. `;
     }
     if (err.hasOwnProperty('message')) {
       errorMessage += err.message;
     }
+    console.log(errorMessage);
+  }
+  else if (err.hasOwnProperty('code')) { 
+    errorMessage += `Error code is: ${err.code}. `;
     console.log(errorMessage);
   }
   else {
