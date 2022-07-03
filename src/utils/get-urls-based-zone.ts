656e0f5e0426a2b1a92b78c0816fafb3db5329c1
Ali TaghiZadeh <ali0tm8@gmail.com>
2022-07-03 17:00:55 +0430

    add sandbox zone

--- src/utils/get-urls-based-zone.ts
+++ src/utils/get-urls-based-zone.ts
@@ -4,45 +4,51 @@ import { Command } from "@oclif/command";
 // Project Modules
 import {
   api_sakku_base_url_khatam,
+  api_sakku_base_url_sandbox,
   api_sakku_base_url_serverius,
   sakku_panel_auth_url_khatam,
   sakku_panel_auth_url_serverius,
+  sakku_panel_auth_url_sandbox,
   webSocket_url_khatam,
+  webSocket_url_sandbox,
   webSocket_url_serverius,
 } from "../consts/urls";
 import {
   sakkuRegRawKhatam,
   sakkuRegRawServerius,
+  sakkuRegRawSandbox,
   sakkuRegUrlKhatam,
   sakkuRegUrlServerius,
+  sakkuRegUrlSandbox,
 } from "../consts/val";
 import { readDatacenterZone } from "./read-datacenter-zone";
 
 export function getBaseUrl(ctx: Command) {
   let zone = readDatacenterZone(ctx);
-  return zone == "khatam"
-    ? api_sakku_base_url_khatam
-    : api_sakku_base_url_serverius;
+  return zone === "khatam" ? api_sakku_base_url_khatam
+    : (zone === "sandbox" ? api_sakku_base_url_sandbox : api_sakku_base_url_serverius);
 }
 
 export function getWebSocketUrl(ctx: Command) {
   let zone = readDatacenterZone(ctx);
-  return zone == "khatam" ? webSocket_url_khatam : webSocket_url_serverius;
+  return zone === "khatam" ? webSocket_url_khatam
+    : (zone === "sandbox" ? webSocket_url_sandbox : webSocket_url_serverius);
 }
 
 export function getPanelAuthUrl(ctx: Command) {
   let zone = readDatacenterZone(ctx);
-  return zone == "khatam"
-    ? sakku_panel_auth_url_khatam
-    : sakku_panel_auth_url_serverius;
+  return zone === "khatam" ? sakku_panel_auth_url_khatam
+    : (zone === "sandbox" ? sakku_panel_auth_url_sandbox : sakku_panel_auth_url_serverius);
 }
 
 export function getSakkuRegUrl(ctx: Command) {
   let zone = readDatacenterZone(ctx);
-  return zone == "khatam" ? sakkuRegUrlKhatam : sakkuRegUrlServerius;
+  return zone === "khatam" ? sakkuRegUrlKhatam
+    : (zone === "sandbox" ? sakkuRegUrlSandbox : sakkuRegUrlServerius);
 }
 
 export function getSakkuRegRaw(ctx: Command) {
   let zone = readDatacenterZone(ctx);
-  return zone == "khatam" ? sakkuRegRawKhatam : sakkuRegRawServerius;
+  return zone === "khatam" ? sakkuRegRawKhatam
+    : (zone === "sandbox" ? sakkuRegRawSandbox : sakkuRegRawServerius);
 }
